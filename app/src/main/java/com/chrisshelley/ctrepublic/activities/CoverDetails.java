060ebad2e9ea7809f040864192a33dbe59dda61a
Chris Shelley <chris@leading2lean.com>
2021-11-22 21:05:40 -0700

    WIP

--- app/src/main/java/com/chrisshelley/ctrepublic/activities/CoverDetails.java
+++ app/src/main/java/com/chrisshelley/ctrepublic/activities/CoverDetails.java
@@ -2,9 +2,13 @@ package com.chrisshelley.ctrepublic.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.chrisshelley.ctrepublic.R;
+import com.chrisshelley.ctrepublic.models.CTRepublic;
 
 public class CoverDetails extends AppCompatActivity {
 
@@ -12,5 +16,23 @@ public class CoverDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cover_details);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        CTRepublic.navigateTo(this, CoverList.class);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        CTRepublic.navigateTo(this, CoverList.class);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_cover_details, menu);
+        return true;
     }
 }
