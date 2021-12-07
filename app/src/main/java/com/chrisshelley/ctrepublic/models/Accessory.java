290cdafdc41cf8e4a5a44f1305f71b6e1f0305f9
Chris Shelley <chris@leading2lean.com>
2021-12-07 00:33:47 -0700

    Complete

--- app/src/main/java/com/chrisshelley/ctrepublic/models/Accessory.java
+++ app/src/main/java/com/chrisshelley/ctrepublic/models/Accessory.java
@@ -1,6 +1,6 @@
 package com.chrisshelley.ctrepublic.models;
 
-public class Accessory  extends CollectionItem {
+public class Accessory extends CollectionItem {
     private String mItemType = CTRepublic.TYPE_ACCESSORY;
 
     public String[] getSubtypeChoices() {
